ae8de92a865f2d58691c9bb8da0f8d108d6f013b
Juan Sebastian Ramirez <juan.solarte@ceiba.com.co>
2023-03-17 20:32:01 -0500

    Implementacion Retrofit

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/data/remote/LocationApi.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/data/remote/LocationApi.kt
@@ -1,11 +1,27 @@
 package co.edu.udea.compumovil.gr03_20231.lab1.data.remote
 
 import co.edu.udea.compumovil.gr03_20231.lab1.domain.ApiAuth
+import co.edu.udea.compumovil.gr03_20231.lab1.domain.City
+import co.edu.udea.compumovil.gr03_20231.lab1.domain.Country
+import co.edu.udea.compumovil.gr03_20231.lab1.domain.State
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface LocationApi {
 
     @GET("getaccesstoken")
-    suspend fun GetToken(@Header("api-token") apiToken: String ): ApiAuth
+    suspend fun getToken(
+        @Header("api-token") apiToken: String,
+        @Header("user-email") user: String
+    ): ApiAuth
+
+    @GET("countries")
+    suspend fun getCountries(@Header("Authorization") token: String): List<Country>
+
+    @GET("states/{country}")
+    suspend fun getStatesByCountry(@Header("Authorization") token: String, @Path("country") country: String): List<State>
+
+    @GET("cities/{state}")
+    suspend fun getCityByState(@Header("Authorization") token: String, @Path("state") state: String): List<City>
 }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/data/service/LocationService.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/data/service/LocationService.kt
@@ -3,25 +3,49 @@ package co.edu.udea.compumovil.gr03_20231.lab1.data.service
 import co.edu.udea.compumovil.gr03_20231.lab1.data.remote.LocationApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import io.github.cdimascio.dotenv.dotenv
 
 class LocationService() {
 
     private val apiKey: String
-    private val baseUrl: String
-    private val retrofit: Retrofit
+    private val userEmail: String
+    private val locationApi: LocationApi
 
     init {
-        val dotenv = DotEnv.load()
-        apiKey = dotenv["API_KEY"]
-        baseUrl = dotenv["BASE_URL"]
-        retrofit = Retrofit.Builder()
+        val env = dotenv {
+            directory = "/assets"
+            filename = "env"
+        }
+        apiKey = env["API_KEY"]
+        val baseUrl = env["BASE_URL"]
+        userEmail = env["EMAIL_API_KEY"]
+        var retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        countryApi = retrofit.create(CountryApi::class.java)
+        locationApi = retrofit.create(LocationApi::class.java)
     }
 
-    suspend fun GetToken(): String {
-        val token = locationApi.GetToken()
+    private suspend fun getToken(): String {
+        val token = locationApi.getToken(apiKey, userEmail)
+        return "Bearer ${token.authToken}"
+    }
+
+    suspend fun getAllCountries(): List<String> {
+        var token = getToken()
+        var countries = locationApi.getCountries(token)
+        return countries.map { it.name }
+    }
+
+    suspend fun getAllStates(country: String): List<String> {
+        var token = getToken()
+        var states = locationApi.getStatesByCountry(token, country)
+        return states.map { it.name }
+    }
+
+    suspend fun getAllCity(state: String): List<String> {
+        var token = getToken()
+        var states = locationApi.getCityByState(token, state)
+        return states.map { it.name }
     }
 }
