42550aa36a59b80f2306f8d225389ead39aa9321
Juan Sebastian Ramirez <juan.solarte@ceiba.com.co>
2023-03-23 12:38:25 -0500

    MConectar y arreglar vistas

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr03_20231/lab1/view/PersonalDataActivity.kt
@@ -0,0 +1,96 @@
+package co.edu.udea.compumovil.gr03_20231.lab1.view
+
+import android.app.DatePickerDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import android.widget.DatePicker
+import androidx.core.content.ContextCompat
+import co.edu.udea.compumovil.gr03_20231.lab1.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+class PersonalDataActivity : AppCompatActivity() {
+
+    private val myCalendar = Calendar.getInstance()
+    private val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    private lateinit var editTextDate: EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_data)
+        editTextDate = findViewById(R.id.labelbirthday)
+
+        setSpinnerEducational()
+
+        setDatePicker()
+
+        setNextButton()
+
+    }
+
+    private fun setNextButton() {
+        val nextButton = findViewById<Button>(R.id.button_next)
+        nextButton.setOnClickListener {
+            val intent = Intent(this, ContactDataActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setSpinnerEducational() {
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position == 0) {
+                    (parent?.getChildAt(0) as? TextView)?.setTextColor(
+                        ContextCompat.getColor(
+                            this@PersonalDataActivity,
+                            R.color.gray
+                        )
+                    )
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.educational_options_array,
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spinner.adapter = adapter
+    }
+
+    private fun setDatePicker() {
+        var buttonDatePicker = findViewById<Button>(R.id.button_date_picker)
+        buttonDatePicker.setOnClickListener {
+            val datePicker = DatePickerDialog(
+                this,
+                dateSetListener,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            )
+
+            datePicker.show()
+        }
+    }
+
+    private val dateSetListener =
+        DatePickerDialog.OnDateSetListener { _: DatePicker?, year: Int, month: Int, dayOfMonth: Int ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            editTextDate.setText(dateFormat.format(myCalendar.time))
+        }
+}
